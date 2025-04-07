141ff40db91117a0db540dcbced69b09f3b3e181
Anson1003487 <1003487@pdsb.net>
2025-04-07 17:56:12 -0400

    Update odd_or_even.py

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,7 +1,19 @@
-a=int(input("please enter nypur first number"))
-b=int(input("please enter youe second number"))
-if a%2==1 and b%2==1:
-  result="odd"
+# Student Name: [Anson Tang]
+# Student Number: [1003487]
+# Course Code: ICS3U
+# Part 1: Even or odd?
+# Variable Dictionary:
+#   num1: First input number (int)
+#   num2: Second input number (int)
+#   result: Product of num1 and num2 (int)
+
+print("Welcome to the even and odd detector!")
+print("This program determines if the product of two whole positive numbers will be even or odd!")
+
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+
+if num1 % 2 == 0 or num2 % 2 == 0:
+    print(f"The product {num1} x {num2} will be an even number.")
 else:
-  result="even"
-print(f"the product {a}*{b} will be an {result} number")
+    print(f"The product {num1} x {num2} will be an odd number.")
