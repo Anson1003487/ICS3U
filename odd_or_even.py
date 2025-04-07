263ce08327d5f644f256fd3696012f7440d01bbf
Anson1003487 <1003487@pdsb.net>
2025-04-07 13:29:36 -0400

    Update odd_or_even.py

--- odd_or_even.py
+++ odd_or_even.py
@@ -5,8 +5,3 @@ if a%2==1 and b%2==1:
 else:
   result="even"
 print(f"the product {a}*{b} will be an {result} number")
-
-example:
-please enter nypur first number 2
-please enter youe second number 5
-the product 2*5 will be an even number
