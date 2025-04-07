6418d7149411d3a4c98399a2fbc5657acd4ca920
Anson1003487 <1003487@pdsb.net>
2025-04-07 17:53:31 -0400

    Update part 3.py

--- part 3.py
+++ part 3.py
@@ -1,3 +1,15 @@
+# Student Name: [Anson Tang]
+# Student Number: [1003487]
+# Course Code: ICS3U
+# Part 3: Making change in coins
+# Variable Dictionary:
+#   cents: Input amount in cents (int)
+#   quarters: Number of quarters (int)
+#   dimes: Number of dimes (int)
+#   nickels: Number of nickels (int)
+#   pennies: Number of pennies (int)
+#   remaining: Remaining cents after each calculation (int)
+
 cents = int(input("Please enter the amount of change in cents: "))
 cents = cents % 100
 
