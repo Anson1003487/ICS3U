e6f753166b7c9231da964f358174c99a5f2ff084
Anson1003487 <1003487@pdsb.net>
2025-04-07 17:49:49 -0400

    Update guessing game ag.py

--- guessing game ag.py
+++ guessing game ag.py
@@ -1,31 +1,37 @@
+
+# Student Name: [Anson Tang
+# Student Number: [1003487]
+# Course Code: ICS3U
+# Programming Assignment 2: A number guessing game
+# Variable Dictionary:
+#   target: The random number to guess (int)
+#   guess: User's current guess (int)
+#   attempts: Number of guesses used (int)
+#   max_attempts: Maximum allowed guesses (int)
+
 import random
 
-def main():
-    target_number = random.randint(1, 100)
-    guess_count = 0
-    max_guesses = 6
+target = random.randint(1, 100)
+attempts = 0
+max_attempts = 6
 
-    print("Hello! Welcome to the number guessing game!")
-    print("I am thinking of a number between 1 and 100. It is your turn to guess what it is.")
-    print(f"You have a maximum of {max_guesses} tries.\n")
+print("Hello! Welcome to the number guessing game!")
+print("I am thinking of a number between 1 and 100.")
+print(f"You have a maximum of {max_attempts} tries.\n")
 
-    while guess_count < max_guesses:
-        guess_count += 1
-        try:
-            user_guess = int(input(f"Guess #{guess_count}: "))
-        except ValueError:
-            print("Please enter a valid number between 1 and 100.")
-            continue
+while attempts < max_attempts:
+    attempts += 1
+    guess = int(input(f"Guess #{attempts}: "))
+    
+    if guess == target:
+        print("\nYou Guessed right!")
+        break
+    elif guess < target:
+        print("Higher!")
+    else:
+        print("Lower!")
 
-        if user_guess < target_number:
-            print("Higher!")
-        elif user_guess > target_number:
-            print("Lower!")
-        else:
-            print("You guessed right!")
-            return
+if attempts == max_attempts and guess != target:
+    print(f"\nSorry: you are out of guesses! The answer was {target}. Better luck next time!")
 
-    print(f"Sorry: you are out of guesses! The answer was {target_number}. Better luck next time!")
-
-if __name__ == "__main__":
-    main()
+    
