f29676b95513debfd8e691433648c570b1ad5746
Anson1003487 <1003487@pdsb.net>
2025-04-08 18:20:52 -0400

    Update odd_or_even.py

--- odd_or_even.py
+++ odd_or_even.py
@@ -7,16 +7,17 @@
 #   num2: Second input number (int)
 #   result: Product of num1 and num2 (int)
 
+# Display welcome message
 print("Welcome to the even and odd detector!")
 print("This program determines if the product of two whole positive numbers will be even or odd!")
-# Display welcome message
 
+# Get user input
 num1 = int(input("Please enter your first number: "))
 num2 = int(input("Please enter your second number: "))
-# Get user input
 
+
+# Check even/odd using modulo
 if num1 % 2 == 0 or num2 % 2 == 0:
     print(f"The product {num1} x {num2} will be an even number.")
 else:
     print(f"The product {num1} x {num2} will be an odd number.")
-# Check even/odd using modulo
