a4be63677330f94c3d5cc40f7b4f421ed69e0ec0
Anson1003487 <1003487@pdsb.net>
2025-04-08 18:21:21 -0400

    Update part 2.py

--- part 2.py
+++ part 2.py
@@ -8,10 +8,11 @@
 
 import math
 
-edge = float(input("Please enter the edge length of your cube: "))
 # Get cube edge length
+edge = float(input("Please enter the edge length of your cube: "))
 
-diagonal = edge * math.sqrt(3)
 # Calculate and print diagonal (edge × √3)
+diagonal = edge * math.sqrt(3)
+
 
 print(f"The length of the inner diagonal of a cube with side length {edge} is: {diagonal:.2f}")
